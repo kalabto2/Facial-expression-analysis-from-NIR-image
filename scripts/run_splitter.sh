b7264af0b1a91532301982b28a4063bbed2ed2ae
Tomáš Kalabis <kalabto2@fit.cvut.cz>
2023-09-02 19:08:28 +0200

    add absolute paths

--- scripts/run_splitter.sh
+++ scripts/run_splitter.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-vl_data_path="../data/B_OriginalImg/VL/Strong"  # data to be split
-ni_data_path="../data/B_OriginalImg/NI/Strong"  # data to be split
+# Get the directory containing the script
+script_dir=$(dirname "$(readlink -f "$0")")
+# Get the parent directory - that is the PROJECT_HOME
+PROJECT_HOME=$(dirname "$script_dir")/
 
-json_train_split_pth="../splits/train_split.json"
-json_test_split_pth="../splits/test_split.json"
-json_val_split_pth="../splits/val_split.json"
+echo "Path to the script: $script_dir"
+echo "PROJECT_HOME directory: $PROJECT_HOME"
+
+vl_data_path=$PROJECT_HOME"data/B_OriginalImg/VL/Strong"  # data to be split
+ni_data_path=$PROJECT_HOME"data/B_OriginalImg/NI/Strong"  # data to be split
+
+json_train_split_pth=$PROJECT_HOME"splits/train_split.json"
+json_test_split_pth=$PROJECT_HOME"splits/test_split.json"
+json_val_split_pth=$PROJECT_HOME"splits/val_split.json"
 
 train_n_img_picked=3
 test_n_img_picked=2
