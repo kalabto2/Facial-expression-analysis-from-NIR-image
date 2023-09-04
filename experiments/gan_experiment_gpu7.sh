5e0e64967349436be3b84e8427aba6a0e89907fb
Tomáš Kalabis <kalabto2@fit.cvut.cz>
2023-09-04 13:03:38 +0200

    Add experiments

--- experiments/gan_experiment_gpu7.sh
+++ experiments/gan_experiment_gpu7.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+# Compared to experiment 6 changed:
+
 # Get the directory containing the script
 script_dir=$(dirname "$(readlink -f "$0")")
 # Get the parent directory - that is the PROJECT_HOME
@@ -8,40 +10,44 @@ PROJECT_HOME=$(dirname "$script_dir")/
 echo "Path to the script: $script_dir"
 echo "PROJECT_HOME directory: $PROJECT_HOME"
 
-source $PROJECT_HOME"/venv/bin/activate"
-
 # ================= PARAMETERS =================
-log_nth_image=1
+log_nth_image=100
 
 # ----------------  Data -----------------------
+train_split=$PROJECT_HOME'splits/preproc_train_split_E.json'
+val_split=$PROJECT_HOME'splits/preproc_val_split_E.json'
 batch_size=1
-shuffle_data=1
-train_split_fp=$PROJECT_HOME"splits/preproc_train_split_E.json"
-val_split_fp=$PROJECT_HOME"splits/preproc_val_split_E.json"
 
 # ---------------- Training --------------------
 epochs=30
 restore_training_from_checkpoint="/" # '/' stands for no restore, else specify path
-num_workers=4
-use_gpu=0
+num_workers=2
+use_gpu=1
 random_seed=1337
 
 # ------------- Architecture -------------------
-n_residual_blocks=6
+n_residual_blocks=9
 lambda_idt=0
-lambda_cycle=6
+lambda_cycle=10
+lambda_discriminator=0.5
 
 # ------------- Optimization -------------------
 train_optim="Adam"
 learning_rate=2e-4
 beta1=0.5
 scheduler_enabled=0
-scheduler_step_freq=1
-scheduler_n_steps=8
+scheduler_step_freq=40
+scheduler_n_steps=1000
 scheduler_eta_min=2e-5
+
+# ------------- Initialization -----------------
+weights_init_std=0.02
+
 # ==============================================
 
-../gan_cli_train.py --batch_size $batch_size \
+../gan_cli_train.py --train_split $train_split \
+                    --val_split $val_split \
+                    --batch_size $batch_size \
                     --learning_rate $learning_rate \
                     --train_optim $train_optim \
                     --epochs $epochs \
@@ -58,6 +64,5 @@ scheduler_eta_min=2e-5
                     --scheduler_step_freq $scheduler_step_freq \
                     --scheduler_n_steps $scheduler_n_steps \
                     --scheduler_eta_min $scheduler_eta_min \
-                    --shuffle_data $shuffle_data \
-                    --train_split_fp $train_split_fp \
-                    --val_split_fp $val_split_fp
+                    --weights_init_std $weights_init_std \
+                    --lambda_discriminator $lambda_discriminator
